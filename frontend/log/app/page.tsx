373f7350204b09d86a0f951a69b6cc85b9f3833f
Hosanna Otchere <hosannaob2@gmail.com>
2026-03-04 14:58:37 -0500

    setup

---
+++ frontend/log/app/page.tsx
@@ -0,0 +1,18 @@
+// Login 
+import Image from "next/image";
+
+export default function Home() {
+  // Welcome Back Message
+  // TODO - Login, so User & Passwrod 
+  /// TODO - Fake get call 
+
+  // TODO - alread 
+  return (
+    <div className="login-container">
+      <h1 className="welcome-back-header"> Welcome Back! </h1>
+      <div className="login-inputs">
+      </div>
+      
+    </div>
+  );
+}
